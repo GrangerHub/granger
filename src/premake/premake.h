0747ebadec222521fee07477bd26f12908afea77
Jeff Kent <jeff@jkent.net>
2016-10-27 23:42:02 -0500

    implemented os.elevate()

--- src/premake/premake.h
+++ src/premake/premake.h
@@ -37,6 +37,7 @@
 #if PLATFORM_WINDOWS
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <shellapi.h>
 #else
 #include <unistd.h>
 #endif
@@ -64,6 +65,7 @@ int path_normalize(lua_State* L);
 int path_translate(lua_State* L);
 int os_chdir(lua_State* L);
 int os_copyfile(lua_State* L);
+int os_elevate(lua_State *L);
 int os_getcwd(lua_State* L);
 int os_getversion(lua_State* L);
 int os_is64bit(lua_State* L);
