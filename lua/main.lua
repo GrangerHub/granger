d35e3847124c924461b4450c2c2f8a55ee8c8cab
Jeff Kent <jeff@jkent.net>
2016-11-13 17:44:12 -0600

    testing file replacement

--- lua/main.lua
+++ lua/main.lua
@@ -4,8 +4,24 @@
 -- Copyright (c) 2016 Jeff Kent <jeff@jkent.net>
 --
 
-require "lib"
+require 'lib'
 
-print("elevate result: " .. tostring(os.elevate()))
+local src-dir = os.getenv('GRANGER_SRC')
+if not src_dir then
+	os.exit()
+end
 
-print("Hello World!")
+local dst_dir = path.getdirectory(_EXE_PATH)
+local tremulous_exe = "tremulous"
+if os.is('windows') then
+	tremulous_exe = tremulous_exe .. '.exe'
+end
+
+local src = path.join(src_dir, tremulous_exe)
+local dst = path.join(dst_dir, tremulous_exe)
+
+if not os.access(dst, 'w') then
+	os.elevate()
+end
+
+os.rename(src, dst)
